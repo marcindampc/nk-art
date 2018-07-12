db94c596f5ed17e3733b3408a5c16fbf02242e32
Marcin Dampc <marcindampc@gmail.com>
2018-07-12 20:43:05 +0100

    Clean-up code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // scroll animation
 
-// $('#mainNav ul li a[href^="#"]').on('click', function(e){
-//     e.preventDefault();
-//     var hashTag = this.hash;
-//     $('html, body').animate({
-//         scrollTop: $(this.hash).offset().top
-//     }, 1000, function () {
-//         window.location.hash = hashTag;
-//     });
-// })
+$('#mainNav ul li a[href^="#"]').on('click', function(e){
+    e.preventDefault();
+    var hashTag = this.hash;
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1000, function () {
+        window.location.hash = hashTag;
+    });
+})
